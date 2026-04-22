d7d983b420666a9dc1881089091400adc5e01d5d
Robert Sköld <robert@publicclass.se>
2026-04-22 18:22:33 +0200

    Update resource versions
    This commit updates the resource versions for `mmmigrate` and `pgfmt` to
    `0.5.0` and `0.3.1` respectively.

--- Formula/mmmigrate.rb
+++ Formula/mmmigrate.rb
@@ -17,11 +17,13 @@ class Mmmigrate < Formula
       sha256 "f1c6275505727c3b1fed8a47cc7336ff45a7e16bc5516970d98ab3729ab8adb8"
 
       resource "mysql" do
+        version "0.5.0"
         url "https://github.com/middle-management/mmmigrate/releases/download/v#{version}/mmmigrate-mysql-darwin-arm64"
         sha256 "6ecf937be9b2cd6d73f0fe19a75258ab029bd8ff1fb93d2da2fbffdeffb3a1a9"
       end
 
       resource "sqlite" do
+        version "0.5.0"
         url "https://github.com/middle-management/mmmigrate/releases/download/v#{version}/mmmigrate-sqlite-darwin-arm64"
         sha256 "e371b6803ce032526e771caa9f59ef2144e4bfdd3d1ca8c124b5a7243c348c84"
       end
@@ -31,11 +33,13 @@ class Mmmigrate < Formula
       sha256 "62a20c23e0867c925a6c06f2e56e9f0e98d542c18c28c11e4f2ef50c3a958866"
 
       resource "mysql" do
+        version "0.5.0"
         url "https://github.com/middle-management/mmmigrate/releases/download/v#{version}/mmmigrate-mysql-darwin-amd64"
         sha256 "1e5bfa177bf6e469897342eefb16e1fa360154b2d3c36df603507c451fa522f0"
       end
 
       resource "sqlite" do
+        version "0.5.0"
         url "https://github.com/middle-management/mmmigrate/releases/download/v#{version}/mmmigrate-sqlite-darwin-amd64"
         sha256 "12d9f7b2f09f1bb9444ffdaf15ee26efff8a2de4ed5b9a7a70d220733db325b8"
       end
@@ -48,11 +52,13 @@ class Mmmigrate < Formula
       sha256 "20916a9788f84c8477bad758bbec2dbacf516ba7e0c8e68e2d498ff303f8077e"
 
       resource "mysql" do
+        version "0.5.0"
         url "https://github.com/middle-management/mmmigrate/releases/download/v#{version}/mmmigrate-mysql-linux-arm64"
         sha256 "8f94d18f7b3fa75858dcd0a330fb7be71159757c5be3876ae03a7a9862fefd36"
       end
 
       resource "sqlite" do
+        version "0.5.0"
         url "https://github.com/middle-management/mmmigrate/releases/download/v#{version}/mmmigrate-sqlite-linux-arm64"
         sha256 "6f3b4fde5011b84276b1396dddd79ca15c56acf01096ecb647e2ad504294a7fe"
       end
@@ -62,11 +68,13 @@ class Mmmigrate < Formula
       sha256 "4150a0af0616cefd7e26861e59b66e9e61ea93345cc6d83d6e8d441376ef5d92"
 
       resource "mysql" do
+        version "0.5.0"
         url "https://github.com/middle-management/mmmigrate/releases/download/v#{version}/mmmigrate-mysql-linux-amd64"
         sha256 "dbb8962be6b2327342c22da11cbc6835e90764c40d5af8997ddf8b540fb5cf05"
       end
 
       resource "sqlite" do
+        version "0.5.0"
         url "https://github.com/middle-management/mmmigrate/releases/download/v#{version}/mmmigrate-sqlite-linux-amd64"
         sha256 "8058111ee4941db1369bcc193f2b33e6e31afb2ae85b9e1c06f2a83b4fb77da8"
       end
--- Formula/pgfmt.rb
+++ Formula/pgfmt.rb
@@ -17,6 +17,7 @@ class Pgfmt < Formula
       sha256 "7a0527a24272cac13d4400d38c966b541efed85073d787fdd787174fa37347f4"
 
       resource "lsp" do
+        version "0.3.1"
         url "https://github.com/middle-management/pgfmt/releases/download/v#{version}/pgfmt-lsp-darwin-arm64"
         sha256 "b2fc4bab808fc4388d4cb13040faaec08f4532e3c3e9eab2a250c171a77d7cee"
       end
@@ -26,6 +27,7 @@ class Pgfmt < Formula
       sha256 "692dd9f9a92fd102a1afdbdd7357277ca7da7456a5cd23e0ea13e06543978e7f"
 
       resource "lsp" do
+        version "0.3.1"
         url "https://github.com/middle-management/pgfmt/releases/download/v#{version}/pgfmt-lsp-darwin-amd64"
         sha256 "7962f0be6ff308e31d7af228cabbb64c1d89704aeaf50b86bea2ef4ab4e7397b"
       end
@@ -38,6 +40,7 @@ class Pgfmt < Formula
       sha256 "27a81b7552221fc2de15cbfcf4b8203b0b42347f56394d261fe30c46a0713335"
 
       resource "lsp" do
+        version "0.3.1"
         url "https://github.com/middle-management/pgfmt/releases/download/v#{version}/pgfmt-lsp-linux-arm64"
         sha256 "527513a6071f14f44773beb0926988994420929e9279ff8f023518caed18c53c"
       end
@@ -47,6 +50,7 @@ class Pgfmt < Formula
       sha256 "95ccc7d6f525b108027350da444c997e341445a0fb79f2b8b4b7d5be68015a32"
 
       resource "lsp" do
+        version "0.3.1"
         url "https://github.com/middle-management/pgfmt/releases/download/v#{version}/pgfmt-lsp-linux-amd64"
         sha256 "a7f95503ef4cf18aeaf6fc94a78541aa7c86850b824ad3a7520df41401eba93a"
       end
